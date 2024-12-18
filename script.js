2d340ec68c739eaad8927bd84450c288afb31ee6
Ali Ashraf <89378903+webashraf@users.noreply.github.com>
2024-12-19 00:14:40 +0600

    Catch all the radio color buttons

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-console.log("Hello world!");
+console.log("Hello world!")
 
+const colorRadios = document.querySelectorAll('input[name="color"]');
+
+// Catch all the radio color button
+colorRadios.forEach(radio => {
+  radio.addEventListener('change', function() {
+    console.log(this.value);  
+  });
+});
