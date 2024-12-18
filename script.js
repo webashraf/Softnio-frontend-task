581490ff21f0be55136692f0eccad3c0424be56d
Ali Ashraf <89378903+webashraf@users.noreply.github.com>
2024-12-19 00:35:37 +0600

    Add click to change image by color functionality

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 console.log("Hello world!")
 
-const colorRadios = document.querySelectorAll('input[name="color"]');
+const images = ["./assets/watch-purple.png", "./assets/watch-cyan.png", "./assets/watch-blue.png", "./assets/watch-black.png"]
 
-// Catch all the radio color button
-colorRadios.forEach(radio => {
-  radio.addEventListener('change', function() {
-    console.log(this.value);  
+
+// Catch all the radio of colors
+const colorRadios = document.querySelectorAll('input[name="color"]');
+// Select the product image element
+const productImage = document.getElementById('productImage');
+
+// Add event listeners to each radio button
+colorRadios.forEach((radio, index) => {
+  radio.addEventListener('change', () => {
+
+    console.log("clicked", productImage);
+    productImage.src = images[index];
   });
 });
